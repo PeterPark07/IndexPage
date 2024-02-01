de6b3be793befae10e0aca301b8aa479a7165aa7
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 15:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ default_collection = "log"  # Set a default collection
 collections_list = ["log", "messages", "torrentlog"]
 mongo_data = db[default_collection].find()  # Fetch data from the default collection
 
+selected_collection = default_collection  # Initialize selected_collection with default value
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    global selected_collection  # Use the global keyword to modify the global variable
+
     if request.method == 'POST':
         selected_collection = request.form.get('collection_name', default_collection)
 
@@ -25,7 +29,7 @@ def home():
         return render_template('entries.html', collections_list=collections_list, selected_collection=selected_collection, entries=entries_data)
 
     # On initial GET request, render the home page with the default collection
-    return render_template('entries.html', collections_list=collections_list, selected_collection=default_collection, entries=mongo_data)
+    return render_template('entries.html', collections_list=collections_list, selected_collection=selected_collection, entries=mongo_data)
 
 @app.route('/delete_entry/<string:entry_id>', methods=['POST'])
 def delete_entry(entry_id):
