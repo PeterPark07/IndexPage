47cd92a3b74a0e453b831d109fe06c127bdc9c41
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-01 15:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ def home():
 
         # Check if the selected collection exists in the database
         if selected_collection not in db.list_collection_names():
-            return render_template('home.html', collections_list=collections_list, error_message=f'Collection "{selected_collection}" not found.')
+            return render_template('entries.html', collections_list=collections_list, error_message=f'Collection "{selected_collection}" not found.')
 
         # Retrieve data from the selected collection
         collection = db[selected_collection]
         entries_data = collection.find()
 
-        return render_template('home.html', collections_list=collections_list, selected_collection=selected_collection, entries=entries_data)
+        return render_template('entries.html', collections_list=collections_list, selected_collection=selected_collection, entries=entries_data)
 
     # On initial GET request, render the home page with the default collection
-    return render_template('home.html', collections_list=collections_list, selected_collection=default_collection, entries=mongo_data)
+    return render_template('entries.html', collections_list=collections_list, selected_collection=default_collection, entries=mongo_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
