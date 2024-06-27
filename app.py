c34e61f44b526c73c6036656789e9b00256f7c6f
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-06-27 15:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def home():
         session['selected_db'] = default_db  # Set default database if not already set
 
     selected_db = session['selected_db']
-    collections_list = collections_map[selected_db]
+    collections_list = collections_map.get(selected_db, [])
 
     if 'selected_collection' not in session or session['selected_collection'] not in collections_list:
         session['selected_collection'] = default_collection  # Set default collection if not already set or invalid
@@ -35,20 +35,19 @@ def home():
         selected_db = request.form.get('db_name', default_db)
         selected_collection = request.form.get('collection_name', default_collection)
 
-        if selected_db in db_list and selected_collection in collections_map[selected_db]:
+        if selected_db in db_list and selected_collection in collections_map.get(selected_db, []):
             session['selected_db'] = selected_db
             session['selected_collection'] = selected_collection
         else:
             # Handle invalid selections gracefully, perhaps with an error message
-            return render_template('entries.html', db_list=db_list, collections_list=collections_list,
+            return render_template('entries.html', db_list=db_list, collections_list=collections_map.get(selected_db, []),
                                    error_message=f'Database "{selected_db}" or collection "{selected_collection}" not found.')
 
     db = mongo_client[session['selected_db']]
     entries_data = list(db[selected_collection].find())  # Limiting the number of entries for display
 
-    return render_template('entries.html', db_list=db_list, collections_list=collections_list,
+    return render_template('entries.html', db_list=db_list, collections_list=collections_map.get(selected_db, []),
                            selected_db=selected_db, selected_collection=selected_collection, entries=entries_data)
-
 
 
 
